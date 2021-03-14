135ee42e726ad99b96936c6b68474f742bc9f496
Naka Masato <masatonaka1989@gmail.com>
2021-03-14 11:19:49 +0900

    first commit

---
+++ src/main/java/com/naka/springbootrabbitmqworkerqueue/SpringBootRabbitmqWorkerQueueApplication.java
@@ -0,0 +1,13 @@
+package com.naka.springbootrabbitmqworkerqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqWorkerQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRabbitmqWorkerQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naka/springbootrabbitmqworkerqueue/SpringBootRabbitmqWorkerQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naka.springbootrabbitmqworkerqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqWorkerQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
